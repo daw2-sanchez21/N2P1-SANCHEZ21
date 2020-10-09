39fe64c009b87e4d9194cb780d9f4b2c3f6d7a2c
daw2-sanchez21 <71894663+daw2-sanchez21@users.noreply.github.com>
2020-10-09 17:43:09 +0200

    Create session1.php

---
+++ session1.php
@@ -0,0 +1,18 @@
+<?php
+session_unset();
+?>
+ <form method="post" action="sessionP2.php">
+   <p>Enter your username: 
+    <input type="text" name="user"/>
+   </p>
+   <p>Enter your password: 
+    <input type="password" name="pass"/>
+   </p>
+   <p>
+    <input type="submit" name="submit" value="Submit"/>
+   </p>
+  </form>
+
+
+ </body>
+</html>
