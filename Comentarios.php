f09acd0adaa511bc305d88632f72e40616d10219
daw2-sanchez21 <71894663+daw2-sanchez21@users.noreply.github.com>
2020-10-09 17:42:41 +0200

    Create Comentarios.php

---
+++ Comentarios.php
@@ -0,0 +1,23 @@
+<?php
+echo"He aprendido a usar los siguientes elementos : ";
+echo '<br/>';
+echo"Variable Get";
+echo '<br/>';
+echo"Variable Session";
+echo '<br/>';
+echo"Variable Post";
+echo '<br/>';
+echo"Variable Cookie";
+echo '<br/>';
+echo"Insertar un link";
+echo '<br/>';
+echo"la funcion urlencode()";
+echo '<br/>';
+echo"Null Controls";
+echo '<br/>';
+echo"la funcion date()";
+echo '<br/>';
+echo"La practica es muy útil pero al principio cuesta de entender, nota: 8";
+echo '<br/>';
+echo"Buena explicación del profesor a medida que avanzaba la practica, nota : 8 ";
+?>
